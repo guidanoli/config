0e8310ad23df95018d3a6e396db1bd91a2694e48
Guilherme Dantas <guidanoli@proton.me>
2023-08-30 14:03:18 -0300

    feat: add nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,38 @@
+-- Neovim configuration file
+-- =========================
+
+-- options
+do
+    local opt = vim.opt
+
+    opt.expandtab = true
+    opt.shiftwidth = 4
+    opt.softtabstop = 4
+    opt.tabstop = 4
+    opt.undodir = vim.fn.stdpath"config" .. "/undo"
+    opt.undofile = true
+end
+
+-- lazy setup
+do
+    local lazypath = vim.fn.stdpath"data" .. "/lazy/lazy.nvim"
+
+    if not vim.loop.fs_stat(lazypath) then
+        vim.fn.system{
+            "git",
+            "clone",
+            "--filter=blob:none",
+            "https://github.com/folke/lazy.nvim.git",
+            "--branch=stable", -- latest stable release
+            lazypath,
+        }
+    end
+
+    vim.opt.rtp:prepend(lazypath)
+
+    local lazy = require"lazy"
+
+    lazy.setup{
+        {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    }
+end
