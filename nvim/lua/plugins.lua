40435a00451fda60c744c89a65c3e5ae19bb65ef
Guilherme Dantas <guidanoli@proton.me>
2024-02-24 01:06:38 -0300

    feat: add syntax plugin for jinja2

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -2,5 +2,6 @@ return {
     "bkad/CamelCaseMotion",
     "farmergreg/vim-lastplace",
     "whonore/Coqtail",
+    "Glench/Vim-Jinja2-Syntax",
     {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
 }
