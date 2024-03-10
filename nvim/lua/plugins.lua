31b1dfecd8aed29a9449740bdf7db1ac5d3c8569
Guilherme Dantas <guidanoli@proton.me>
2024-03-10 12:38:51 -0300

    feat: add `leafgarland/typescript-vim`
    - sort plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,8 +1,9 @@
 return {
-    "bkad/CamelCaseMotion",
-    "farmergreg/vim-lastplace",
-    "whonore/Coqtail",
     "Glench/Vim-Jinja2-Syntax",
+    "bkad/CamelCaseMotion",
     "cstrahan/vim-capnp",
+    "farmergreg/vim-lastplace",
+    "leafgarland/typescript-vim",
+    "whonore/Coqtail",
     {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
 }
