edded0428a3bb5650c0d7dca8c68dc0290cdc8f8
Guilherme Dantas <guidanoli@proton.me>
2024-03-22 15:13:11 -0300

    feat: add key mappings for lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -65,4 +65,46 @@ end
 do
     local lspconfig = require"lspconfig"
     lspconfig.tsserver.setup{}
+
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+      callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+
+        local map = function (mode, lhs, rhs)
+            vim.keymap.set(mode, lhs, rhs, opts)
+        end
+
+        local nmap = function (lhs, rhs)
+            map('n', lhs, rhs)
+        end
+
+        local nvmap = function (lhs, rhs)
+            map({'n', 'v'}, lhs, rhs)
+        end
+
+        nmap('gD', vim.lsp.buf.declaration)
+        nmap('gd', vim.lsp.buf.definition)
+        nmap('K', vim.lsp.buf.hover)
+        nmap('gi', vim.lsp.buf.implementation)
+        nmap('<C-k>', vim.lsp.buf.signature_help)
+        nmap('<space>wa', vim.lsp.buf.add_workspace_folder)
+        nmap('<space>wr', vim.lsp.buf.remove_workspace_folder)
+        nmap('<space>wl', function()
+          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end)
+        nmap('<space>D', vim.lsp.buf.type_definition)
+        nmap('<space>rn', vim.lsp.buf.rename)
+        nvmap('<space>ca', vim.lsp.buf.code_action)
+        nmap('gr', vim.lsp.buf.references)
+        nmap('<space>f', function()
+          vim.lsp.buf.format { async = true }
+        end)
+      end,
+    })
 end
