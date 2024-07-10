80b62f9cfd3946d4ec2429b77fc2694583a4c1f7
Guilherme Dantas <guidanoli@proton.me>
2024-07-09 22:02:58 -0300

    Add `clangd` and `gopls` LSP

--- nvim/init.lua
+++ nvim/init.lua
@@ -73,7 +73,22 @@ end
 -- lsp
 do
     local lspconfig = require"lspconfig"
+    local util = require'lspconfig.util'
     lspconfig.tsserver.setup{}
+    lspconfig.clangd.setup{}
+    lspconfig.gopls.setup{
+        root_dir = function(fname)
+            -- see: https://github.com/neovim/nvim-lspconfig/issues/804
+            local mod_cache = vim.trim(vim.fn.system 'go env GOMODCACHE')
+            if fname:sub(1, #mod_cache) == mod_cache then
+                local clients = vim.lsp.get_active_clients { name = 'gopls' }
+                if #clients > 0 then
+                    return clients[#clients].config.root_dir
+                end
+            end
+            return util.root_pattern 'go.work'(fname) or util.root_pattern('go.mod', '.git')(fname)
+        end,
+    }
 
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('UserLspConfig', {}),
